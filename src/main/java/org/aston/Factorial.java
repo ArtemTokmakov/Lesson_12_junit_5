5cbb407a525b315c58ef2e935cfa620f7d972939
Artem Tokmakov <tematokmakov@yandex.ru>
2024-06-10 21:52:33 +0300

    done3

--- src/main/java/org/aston/Factorial.java
+++ src/main/java/org/aston/Factorial.java
@@ -6,6 +6,9 @@ public class Factorial {
         if (n == 0) {
             return 1;
         }
+        if (n < 0) {
+            throw new IllegalArgumentException("Число должно быть положительным");
+        }
         return n * calculateFactorial(n - 1);
     }
 }
--- src/test/java/org/aston/FactorialTest.java
+++ src/test/java/org/aston/FactorialTest.java
@@ -1,24 +1,46 @@
 package org.aston;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 
 class FactorialTest {
 
+    @DisplayName("Проверка факториала для положительных чисел")
+    @ParameterizedTest(name = "Факториал {0} равен {1}")
+    @MethodSource("provideFactorialData")
+    void testCalculateFactorial(int n, int expected) {
+        assertEquals(expected, Factorial.calculateFactorial(n));
+    }
+
+    static Stream<Arguments> provideFactorialData() {
+        return Stream.of(
+                Arguments.of(0, 1),
+                Arguments.of(1, 1),
+                Arguments.of(2, 2),
+                Arguments.of(3, 6),
+                Arguments.of(4, 24),
+                Arguments.of(5, 120),
+                Arguments.of(6, 720),
+                Arguments.of(7, 5040),
+                Arguments.of(8, 40320),
+                Arguments.of(9, 362880),
+                Arguments.of(10, 3628800)
+        );
+    }
+
     @Test
-    public void testCalculateFactorial() {
-        assertEquals(1, Factorial.calculateFactorial(0));
-        assertEquals(1, Factorial.calculateFactorial(1));
-        assertEquals(2, Factorial.calculateFactorial(2));
-        assertEquals(6, Factorial.calculateFactorial(3));
-        assertEquals(24, Factorial.calculateFactorial(4));
-        assertEquals(120, Factorial.calculateFactorial(5));
-        assertEquals(720, Factorial.calculateFactorial(6));
-        assertEquals(5040, Factorial.calculateFactorial(7));
-        assertEquals(40320, Factorial.calculateFactorial(8));
-        assertEquals(362880, Factorial.calculateFactorial(9));
-        assertEquals(3628800, Factorial.calculateFactorial(10));
+    @DisplayName("Проверка на отрицательные числа")
+    void testCalculateFactorialWithNegativeNumbers() {
+        assertThrows(IllegalArgumentException.class, () -> Factorial.calculateFactorial(-1));
     }
 }
